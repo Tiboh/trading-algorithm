37ee5df8a8814a08dccc5a0b232696e3bcd2f39a
DESKTOP-ASUS\Thibault <thibault_85_h@hotmail.fr>
2017-11-30 22:25:56 +0100

    add npm json-query add exchangeRateFee add Acount class

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
-var msg = 'Hello World';
-console.log(msg);
 const Data = require('./data.js');
-var data = new Data();
-console.log(data.getBuyPrice(0));
+
+ALGORITHMS = {
+    'simple': Simple,
+    'pirate': Pirate,
+    'random': Random,
+    'static': Static,
+}
+
+function getTransactionFeeUSD(amoutUSD){
+    if(amoutUSD <= 10){
+        return 0.99;
+    }else if(amountUSD > 11 & amoutUSD <= 25){
+        return 1.49;
+    }else if(amountUSD > 25 & amoutUSD <= 50){
+        return 1.99;
+    }else if(amountUSD > 51 & amoutUSD <= 201){
+        return 2.99;
+    }
+    return amountUSD*1.49/100;   
+}
+
+function Account(fundsUSD){
+    this.fundsUSD = fundsUSD;
+    this.fundsCrypto = 0;
+
+    this.buy = function(amountUSD, exchangeRateUSD){
+        this.fundsUSD -= amountUSD;
+        this.fundsCrypto += (amountUSD/exchangeRateUSD)-(getTransactionFeeUSD(amoutUSD)/exchangeRateUSD);
+    }
+    this.sell = function(amountUSD, exchangeRateUSD){
+        this.fundsUSD += amountUSD-getTransactionFeeUSD(amountUSD);
+        this.fundsCrypto -= amountUSD/exchangeRateUSD;
+    }
+
+    this.balanceUSD = function(exchangeRateUSD){
+        return fundsCrypto*exchangeRateUSD;
+    }
+}
+
+function RunAlgo(algorithmName, csvFile, startDate, endDate) {
+    var data = new Data();
+    var algo = ALGORITHMS.get(algorithmName);
+    
+    
+
+    console.log(data.getBuyPrice('2017-01-17', '2017-02-19'));
+}
--- data.js
+++ data.js
@@ -1,12 +1,15 @@
 var loader = require('csv-load-sync');
+var jsonQuery = require('json-query')
 
-function Data(filename='csv/BTC.csv'){
+function Data(filename='csv/AMZN.csv'){
     this.filename=filename;
     this.history = loader(filename);
 };
 
-Data.prototype.getBuyPrice = function(index){
-    return this.history[index];
+Data.prototype.getBuyPrice = function(startDate, endDate){
+    return jsonQuery('[* Date <= '+startDate+' & Date <= '+endDate+'].Close', {
+    data: this.history
+    }).value       
 };
 
 module.exports = Data;
